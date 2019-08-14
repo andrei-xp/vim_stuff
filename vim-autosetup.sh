e4ad52031f538846a1a6844ce80844fdf38abfad
Andrew <fluffybastard@users.noreply.github.com>
2019-08-14 11:58:26 +0300

    Update vim-autosetup.sh

--- vim-autosetup.sh
+++ vim-autosetup.sh
@@ -1,36 +1,39 @@
 VIM_CONFIG_DIR="${HOME}/.vim"
-DATE=$(date '+%Y-%m-%d %H:%M:%S')
+DATE=$(date '+%Y-%m-%d_%H:%M:%S')
 
 function install_pathogen() {
-    [ ! -d ${VIM_CONFIG_DIR} ] &&                                               \ 
-        mkdir -p ~/.vim/autoload ~/.vim/bundle &&                               \
-        curl -LSso ~/.vim/autoload/pathogen.vim https://tpo.pe/pathogen.vim ||  \ 
-        curl -LSso ~/.vim/autoload/pathogen.vim https://tpo.pe/pathogen.vim
+    [ ! -d ${VIM_CONFIG_DIR} ] && \
+    mkdir -p ~/.vim/autoload ~/.vim/bundle && \
+    curl -LSso ~/.vim/autoload/pathogen.vim https://tpo.pe/pathogen.vim || \
+    curl -LSso ~/.vim/autoload/pathogen.vim https://tpo.pe/pathogen.vim
 }
 
 function create_colors_dir() {
     [ -d ${VIM_CONFIG_DIR} ] && \
         mkdir -p ~/.vim/colors
+    
+    git clone https://github.com/tomasr/molokai.git     ~/.vim/colors/molokai
 }
 
 function install_bundles() {
 
-    git clone git://github.com/jiangmiao/auto-pairs.git             ~/.vim/bundle/auto-pairs
-    git clone git://github.com/Yggdroot/indentLine.git              ~/.vim/bundle/indentLine
-    git clone git@github.com:vim-airline/vim-airline.git            ~/.vim/bundle/vim-airline
-    git clone git@github.com:enricobacis/vim-airline-clock.git      ~/.vim/bundle/vim-airline-clock
-    git clone git@github.com:tpope/vim-commentary.git               ~/.vim/bundle/vim-commentary.git
-    git clone git@github.com:thaerkh/vim-indentguides.git           ~/.vim/bundle/vim-indentguides.git
-    git clone git@github.com:jeffkreeftmeijer/vim-numbertoggle.git  ~/.vim/bundle/vim-numbertoggle
-    git clone git@github.com:sheerun/vim-polyglot.git               ~/.vim/bundle/vim-polyglot
-    git clone git@github.com:tpope/vim-sensible.git                 ~/.vim/bundle/vim-sensible
-    git clone git@github.com:reedes/vim-colors-pencil.git           ~/.vim/bundle/vim-colors-pencil
+    git clone https://github.com/jiangmiao/auto-pairs.git               ~/.vim/bundle/auto-pairs
+    git clone https://github.com/Yggdroot/indentLine.git                ~/.vim/bundle/indentLine
+    git clone https://github.com/vim-airline/vim-airline.git            ~/.vim/bundle/vim-airline
+    git clone https://github.com/enricobacis/vim-airline-clock.git      ~/.vim/bundle/vim-airline-clock
+    git clone https://github.com/tpope/vim-commentary.git               ~/.vim/bundle/vim-commentary.git
+    git clone https://github.com/thaerkh/vim-indentguides.git           ~/.vim/bundle/vim-indentguides.git
+    git clone https://github.com/jeffkreeftmeijer/vim-numbertoggle.git  ~/.vim/bundle/vim-numbertoggle
+    git clone https://github.com/sheerun/vim-polyglot.git               ~/.vim/bundle/vim-polyglot
+    git clone https://github.com/tpope/vim-sensible.git                 ~/.vim/bundle/vim-sensible
+    git clone https://github.com/reedes/vim-colors-pencil.git           ~/.vim/bundle/vim-colors-pencil
 }
 
 function create_vimrc() {
-    [ -f ~/.vimrc ] && \
-        mv ~/.vimrc{,.${DATE}} && \
-        tee ~/.vimrc << EOF
+    [ -f ~/.vimrc ] || \
+    [ ! -f ~/.vimrc] && \
+    mv ~/.vimrc{,.${DATE}} && \
+    tee ~/.vimrc << EOF
 execute pathogen#infect()
 
 " Sane defaults
@@ -110,3 +113,4 @@ EOF
 install_pathogen
 create_colors_dir
 install_bundles
+create_vimrc
